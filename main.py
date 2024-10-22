6aeef169744c2d5b8c871d0f902d1082c3a42831
HOUDA KHADIRI <161170011+hudakhadiri@users.noreply.github.com>
2024-10-23 00:28:01 +0100

    Create main.py
    the streamlit first app

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+
+def main():
+
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("ask question")
+
+    with st.sidebar:
+        st.subheader("your docs")
+        st.file_uploader("upload file")
+        st.button("process")
+
+
+if __name__ == '__main__':
+    main()
